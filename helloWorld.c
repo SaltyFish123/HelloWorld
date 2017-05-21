337ea49a603b8356b6b3582b7012fb576bb26019
Endlesshole <413486687@qq.com>
2017-05-21 21:21:08 +0800

    Change for *.c file
    This is a change for the helloWorld.c and helloWorld.exe file

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+#define SIZE 5
 int main(void) {
+	int i;
+	for (i = 0; i < SIZE; i++) {
+		printf("Well this is the new change.\n");
+	}
   printf("I am stupid.\n");
   return 0;
 }
